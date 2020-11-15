7281c6a2603a8f24b7f7b7db23e1fde28fe84b97
Frido <archalois3004@gmail.com>
2020-11-16 05:21:44 +0700

    splash screen

---
+++ app/src/main/java/com/example/movieapptry/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.movieapptry;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int loading_time = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, loading_time);
+    }
+}
